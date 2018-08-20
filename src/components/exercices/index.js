5e19c792d30e7bc3816e0731efdca45e43040570
Frederic <fredydope@gmail.com>
2018-08-20 11:56:36 +0200

    separation de la page en left and right avec mui

--- src/components/exercices/index.js
+++ src/components/exercices/index.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Grid } from '@material-ui/core/'
+import LeftSide from './LeftSide'
+import RightSide from './RightSide'
+
+
+const styles = {
+	Paper: { padding: 20, marginTop: 10, marginBottom: 10 }
+}
+
 
 class Exercices extends Component {
   render() {
     return (
-      <div>
-          <h1>Exercices</h1>
-      </div>
-    );
+      <Grid container>
+      	<Grid item sm>
+      		<LeftSide styles={styles} />
+      	</Grid>
+      	<Grid item sm>
+      		<RightSide styles={styles} />
+      	</Grid>
+          
+      </Grid>
+    )
   }
 }
 
